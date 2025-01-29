6040eace363dab435b75728803d2ec145265d50a
Alexander Yoshino <alexanderyoshino@me.com>
2025-01-29 17:45:31 -0500

    aliases

---
+++ .zshrc.sh
@@ -0,0 +1,14 @@
+# Aliases
+alias vim="nvim"
+alias nr="npm run"
+alias nrd="npm run dev"
+alias dcdn='docker-compose down'
+alias dcup='docker-compose up'
+
+# Plugins
+plugins=(
+  git
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+  autojump
+)
